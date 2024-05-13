e744c45f7619aca1c91d4012a8b3e5c685b62f27
Nelson <nelsonchenglin@gmail.com>
2024-05-13 16:59:53 -0400

    edited dataservice file to contain more fields

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,8 +7,19 @@ import fetchFromSpotify from "src/services/api";
 export class DataService {
   constructor() {}
 
-  token: string = "";
-  selectedGenre: string = "";
   numberOfChoices: number = 2;
   numberOfQuestions: number = 1;
+  genres: string[] = ["House", "Alternative", "J-Rock", "R&B"];
+  selectedGenre: string = "";
+  albumQuery: string = "";
+  artistQuery: string = "";
+  genreQuery: string = "";
+  authLoading: boolean = false;
+  configLoading: boolean = false;
+  token: string = "";
+  artistId: string = "";
+  albumId: string = "";
+  searchType: string = "genre";
+  searchQuery: string = "";
+  tracks: Track[] = [];
 }

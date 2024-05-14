5e92a347ded8a0c59d394e83444d1dd88e850410
Simrath Randhawa <srandhawa1250@gmail.com>
2024-05-14 17:43:10 -0500

    new branch w functional code for the genre music game

--- src/app/results/results.component.ts
+++ src/app/results/results.component.ts
@@ -1,6 +1,13 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
+interface LeaderboardEntry {
+  name: string;
+  score: number;
+  percentage: number;
+}
+
+
 @Component({
   selector: 'app-results',
   templateUrl: './results.component.html',
@@ -9,6 +16,8 @@ import { Router } from '@angular/router';
 export class ResultsComponent implements OnInit {
   score: number = 0;
   numQuestions: number = 0;
+  resultMessage: string = '';
+  leaderboard: LeaderboardEntry[] = [];
 
   constructor(private router: Router) {
     const navigation = this.router.getCurrentNavigation();
@@ -17,7 +26,40 @@ export class ResultsComponent implements OnInit {
     this.numQuestions = state.numQuestions;
   }
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.calculateResult();
+    this.updateLeaderboard();
+    this.loadLeaderboard();
+  }
+
+  calculateResult(): void {
+    const percentage = (this.score / this.numQuestions) * 100;
+    this.resultMessage = percentage >= 70 ? 'You Win!' : 'You Lose!';
+  }
+
+  updateLeaderboard(): void {
+    const playerName = prompt("Enter your name:");
+    const percentage = (this.score / this.numQuestions) * 100;
+    const newEntry: LeaderboardEntry = {
+      name: playerName || 'Anonymous',
+      score: this.score,
+      percentage: percentage
+    };
+    this.leaderboard.push(newEntry);
+    this.leaderboard.sort((a, b) => b.percentage - a.percentage);
+    this.saveLeaderboard();
+  }
+
+  saveLeaderboard(): void {
+    localStorage.setItem('leaderboard', JSON.stringify(this.leaderboard));
+  }
+
+  loadLeaderboard(): void {
+    const storedLeaderboard = localStorage.getItem('leaderboard');
+    if (storedLeaderboard) {
+      this.leaderboard = JSON.parse(storedLeaderboard);
+    }
+  }
 
   restart() {
     this.router.navigate(['/']);

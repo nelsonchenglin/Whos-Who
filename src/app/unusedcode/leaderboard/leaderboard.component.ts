a39996fd8d00a0774065b828576cd1fad3e8c991
Simrath Randhawa <srandhawa1250@gmail.com>
2024-05-15 10:23:29 -0500

    10:22 5-15 now on main branch code is functional for the album and genre game genre game includes sound. Want to work on maki ng sure the music stops playing anytime you go to the next question and change the page or when the game is done amongst a few other things still

---
+++ src/app/unusedcode/leaderboard/leaderboard.component.ts
@@ -0,0 +1,16 @@
+// import { Component, OnInit, Input } from "@angular/core";
+
+// @Component({
+//   selector: "app-leaderboard",
+//   templateUrl: "./leaderboard.component.html",
+//   styleUrls: ["./leaderboard.component.css"],
+// })
+// export class LeaderboardComponent implements OnInit {
+//   constructor() {}
+
+//   ngOnInit(): void {}
+
+//   @Input() score: number = 0;
+
+//   leaderboard: object = {};
+// }

c162b95baa8028bb10762b8bc877b79e47ac720a
Nelson <nelsonchenglin@gmail.com>
2024-05-16 00:22:51 -0600

    home page css done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,26 @@ import { RouterModule, Routes } from "@angular/router";
 import { AppComponent } from "./app.component";
 import { HomeComponent } from "./home/home.component";
 import { SetUpGameConfigurationComponent } from "./set-up-game-configuration/set-up-game-configuration.component";
-import { GameComponent } from './game/game.component';
-import { ResultsComponent } from './results/results.component';
+import { GameComponent } from "./game/game.component";
+import { ResultsComponent } from "./results/results.component";
+import { NavbarComponent } from "./navbar/navbar.component";
 
 const routes: Routes = [
-  { path: '', component: SetUpGameConfigurationComponent },
-  { path: 'game', component: GameComponent },
-  { path: 'results', component: ResultsComponent }
+  { path: "", component: SetUpGameConfigurationComponent },
+  { path: "game", component: GameComponent },
+  { path: "results", component: ResultsComponent },
+  // {path: 'leaderboard', component: LeaderboardComponent}
 ];
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, SetUpGameConfigurationComponent, GameComponent, ResultsComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    SetUpGameConfigurationComponent,
+    GameComponent,
+    ResultsComponent,
+    NavbarComponent,
+  ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes)],
   providers: [],
   bootstrap: [AppComponent],
